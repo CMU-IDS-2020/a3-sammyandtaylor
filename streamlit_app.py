670a26c3d6827c24c3655d71f2b5bf0426459692
Taylor Sullivan <68781329+tvandaff@users.noreply.github.com>
2020-10-22 10:06:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,27 @@
 import streamlit as st
+import math
 import pandas as pd
 import numpy as np
-#from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import OrdinalEncoder
-from matplotlib import pyplot as plt
-import seaborn as sns
-#from scipy.stats import shapiro
-from scipy import stats
-from matplotlib import pyplot
-#from scipy.stats import boxcox
 from numpy import cov
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot as plt
+from scipy.stats import shapiro
+from scipy import stats
+from scipy.stats import boxcox
 from scipy.stats import pearsonr 
 import statsmodels.api as sm
-from statsmodels.formula.api import ols
-from sklearn.linear_model import LinearRegression
-from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
-from statsmodels.tsa.holtwinters import SimpleExpSmoothing
-from statsmodels.tsa.ar_model import AutoReg
-from sklearn.metrics import mean_squared_error
-import math
+from statsmodels.tsa.api import SimpleExpSmoothing
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import adfuller
 
 
-st.title('This is our new Project')
+st.title('A Timeseries Analysis of Opioid deaths in the U.S.')
 df = pd.read_csv("opioid.csv")
-if st.checkbox('Display Opiod Data'):
+if st.checkbox('Display Opioid Data'):
     st.write(df.head(10))
 
 #I. Exploratory Data Analysis
@@ -32,10 +30,10 @@ if st.checkbox('Display Opiod Data'):
     #b. Determine the Categorical and Continuous Variables
         # Continuous: Income Unemployment NonUSBorn
         # Prediction for CountbyType 
-idvariable = ['ID']
-targetvariable = ['CountByType']
-Continuousvar = ['Income', 'Unemployment', 'NonUSBorn']
-allnum = ['Income', 'Unemployment', 'NonUSBorn', 'CountByType']
+#idvariable = ['ID']
+#targetvariable = ['CountByType']
+#Continuousvar = ['Income', 'Unemployment', 'NonUSBorn']
+#allnum = ['Income', 'Unemployment', 'NonUSBorn', 'CountByType']
     #c. Univariate Analysis: 
 #df.describe()
 #df.info()
@@ -122,20 +120,20 @@ allnum = ['Income', 'Unemployment', 'NonUSBorn', 'CountByType']
 #regressor.score(X_test, y_test)
     #This model only has .13% predictive power
 #V. Time Series Forecasting
-df['TotalCount'] = df['TotalCount'].fillna((df['TotalCount'].median()))
+#df['TotalCount'] = df['TotalCount'].fillna((df['TotalCount'].median()))
 #df['Year'].value_counts()
-newdf = df['TotalCount']
-newdf.index = df.Year
-newdf.head()
-training = newdf[newdf.index <= 2014]
-test = newdf[newdf.index >= 2014]
-training.index = pd.to_datetime(training.index, format='%Y')
-training = training.resample('Y').count()
-test.index = pd.to_datetime(test.index, format='%Y')
-test = test.resample('Y').count()
+#newdf = df['TotalCount']
+#newdf.index = df.Year
+#newdf.head()
+#training = newdf[newdf.index <= 2014]
+#test = newdf[newdf.index >= 2014]
+#training.index = pd.to_datetime(training.index, format='%Y')
+#training = training.resample('Y').count()
+#test.index = pd.to_datetime(test.index, format='%Y')
+#test = test.resample('Y').count()
     #First, a plot of the data 
-training.plot(figsize=(20,14), title= 'Death Count by Year', fontsize=16)
-test.plot(figsize=(20,14), title= 'Death Count by Year', fontsize=16)
+#training.plot(figsize=(20,14), title= 'Death Count by Year', fontsize=16)
+#test.plot(figsize=(20,14), title= 'Death Count by Year', fontsize=16)
     #Now some exponential smoothing 
 #y_hat_avg = test.copy()
 #model = SimpleExpSmoothing(np.asarray(training))
@@ -149,12 +147,58 @@ test.plot(figsize=(20,14), title= 'Death Count by Year', fontsize=16)
 #plt.legend(loc='best')
 #plt.show()
     #Use Model for Forecasting
-y_hat_avg = test.copy()
-fit1 = ExponentialSmoothing(np.asarray(training), trend='add', seasonal_periods=1).fit()
-y_hat_avg['Holt_Winter'] = fit1.forecast(len(test))
-plt.figure(figsize=(16,8))
-plt.plot(training, label='Train')
-plt.plot(test, label='Test')
-plt.plot(y_hat_avg['Holt_Winter'], label='Holt_Winter')
-plt.legend(loc='best')
-plt.show()
+    #Reduce trend with log transformation 
+#train_log = np.log(training)
+#plt.plot(train_log)
+    #Differencing for a particular time log
+#diffinlog = train_log - train_log.shift()
+#plt.plot(diffinlog)
+#model = ARIMA(train_log, order=(2, 1, 0))  
+#fitted = model.fit(disp=-1)  
+#predicted_result = pd.Series(fitted.fittedvalues, copy=True)
+#cumulative_results = predicted_result.cumsum()
+#predicted_ARIMA = pd.Series(train_log[0], index=train_log.index)
+#ARIMA_log = predicted_ARIMA.add(cumulative_results,fill_value=0)
+#ARIMA_log.head()
+#predictions = np.exp(ARIMA_log)
+#plt.plot(training)
+#plt.plot(predictions_ARIMA)
+#RMSE = np.sqrt(sum((predictions-training)**2)/len(training))
+#plt.title(f'RMSE is {RMSE}')
+    #This is an okay model but could be better 
+    #Now on to forecasting
+#print(adfuller(training.dropna())[1])
+    #Stationary timeseries
+    #The problem was assumed necessity for differencing
+#model = ARIMA(training, order = (0,1,1))
+#results = model.fit()
+#resultplot = results.plot_predict(1,50)
+
+if df is not None:
+    df['TotalCount'] = df['TotalCount'].fillna((df['TotalCount'].median()))
+    newdf = df['TotalCount']
+    newdf.index = df.Year
+    newdf.head()
+    training = newdf[newdf.index <= 2014]
+    test = newdf[newdf.index >= 2014]
+    training.index = pd.to_datetime(training.index, format='%Y')
+    training = training.resample('Y').count()
+    test.index = pd.to_datetime(test.index, format='%Y')
+    test = test.resample('Y').count()
+    model = ARIMA(training, order = (0,1,1))
+    results = model.fit()
+    resultplot = results.plot_predict(1,50)
+
+choose_model = st.sidebar.selectbox("Choose Machine Learning Model", 
+['Show Forecasting Results', 'Show Regression Analysis'])
+if (choose_model == "Show Forecasting Results"):
+    st.markdown("## **Forecasting Opioid Deaths through 2050**")
+    st.markdown("### Predicted total deaths from Methadone, Heroine, and Opioids")
+    if not st.checkbox('Hide Graph', False, key=1):
+        st.write(resultplot)
+
+if (choose_model == "Show Regression Analysis"):
+    st.markdown("## **Regression Analysis of Predictor Variables vs. Target*")
+    st.markdown("### Predictor Variables in this case are: Income, Unemployment, Non_US_Born, Bachelor_Degree, Grad_Degree, HS_Grad, Less_Than_HS, Associates_Degree")
+    if not st.checkbox('Hide Graph', False, key=1):
+        st.write(resultplot)
